bb9ad6a75b00b32a074e7b15a483f0e75b0c37ed
Ronald Pe#a <ronald.micros@gmail.com>
2023-02-04 14:56:05 -0300

    Reading 4Geeks Examples - My first commit

---
+++ Domain Name Generator/ForIn.js
@@ -0,0 +1,6 @@
+// 4Geeks Example For...In...
+
+var myArray = [3423, 5, 4, 47889, 654, 8, 867543, 23, 48, 56432, 55, 23, 25, 12];
+  for (var index in myArray) {
+    console.log(`Valor : ${myArray[index]}`);
+  }
---
+++ Domain Name Generator/sort&Reverse.js
@@ -0,0 +1,8 @@
+// 4Geeks Example  Sort & Reverse function
+
+var fruits = ["Banana", "Orange", "Apple",  "Mango"];
+  fruits.sort();
+  console.log("Sort Function: "  + fruits);
+
+  fruits.reverse();
+  console.log("Reverse Function: " + fruits);
